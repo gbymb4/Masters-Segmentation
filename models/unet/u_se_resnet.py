eaf5b27cdb062dc8246e5af27268632e4d98be73
gbymb4 <82245621+gbymb4@users.noreply.github.com>
2023-12-30 16:06:41 +1300

    minor fix

--- models/unet/u_se_resnet.py
+++ models/unet/u_se_resnet.py
@@ -14,6 +14,11 @@ class U_SE_Resnet2D(nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__()
         
+        if 'img_channels' in kwargs.keys():
+            self.channels = kwargs['img_channels']
+        else:
+            self.channels = 1
+        
         self.features = smp.Unet(encoder_name='se_resnet50', *args, **kwargs)
         
         
@@ -21,7 +26,7 @@ class U_SE_Resnet2D(nn.Module):
     def forward(self, x):
         shape = x.shape
         
-        x = x.reshape(-1, 1, *shape[-2:])
+        x = x.reshape(-1, self.channels, *shape[-2:])
         
         y = self.features(x)
         y = y.reshape(shape[0], 2, *shape[2:])
